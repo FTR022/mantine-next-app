b63012765f6a04ec9a7288392a4ce51de63ece1b
augustin <azzerty23@hotmail.fr>
2024-05-29 14:47:31 +0200

    test nested css

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { HeroImageRight } from '@/components/HeroImageRight/HeroImageRight';
 
 export default function HomePage() {
   return (
     <>
+      <HeroImageRight />
       <Welcome />
       <ColorSchemeToggle />
     </>
